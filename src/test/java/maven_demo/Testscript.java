8c143c8a76e36dbd4ed771c3c3cdad811ecf9c8d
SunkaraRam <sunkararam1166@gmail.com>
2023-07-25 18:15:29 +0530

    test script 1

---
+++ src/test/java/maven_demo/Testscript.java
@@ -0,0 +1,12 @@
+package maven_demo;
+
+public class Testscript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("test case 1");
+	}
+
+}
